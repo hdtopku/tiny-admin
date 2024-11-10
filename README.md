16a9f992700552d11eef3be524386278791d4534
hdtopku <1450948930@qq.com>
2024-11-10 22:36:16 +0000

    Prepare for the production

---
+++ README.md
@@ -0,0 +1,30 @@
+## ☘️ Abstract
+Tiny Admin is a full-stack web application
+- 🍁 Front-end framework: [Vue3.5](https://cn.vuejs.org/), [TailwindCSS](https://tailwindcss.com/), [Pinia](https://pinia.vuejs.org/)
+- 🍂 Back-end framework: [SpringBoot3.2.5](https://spring.io/), [Mysql](https://www.mysql.com/), [Mybatis](https://mybatis.org/mybatis-3/)
+- - 🍃 Username: **admin**，Password: **123456**
+- [🪹 github repository](https://github.com/hdtopku/tiny-admin)
+
+## System Design
+1. User Management: User login, logout, user information modification, user role management, user permission management
+2. Role Management: Role creation, role modification, role deletion, role permission management
+3. Permission Management: Permission creation, permission modification, permission deletion
+4. Menu Management: Menu creation, menu modification, menu deletion, menu permission management
+
+## Environment and Deployment
+1. [pnpm](https://github.com/pnpm/pnpm/) (>= 8.7.0, recommended. It is used as the package manager)
+2. [Node.js](https://nodejs.org/) (Node.js version >= 18.12.0，npm version >= 6.14.13)
+3. [MySQL](https://www.mysql.com/) (MySQL version >= 8.0.27)
+4. [Git](https://git-scm.com/) (Git version >= 2.34.1)
+5. [Nginx](https://nginx.org/) (Nginx version >= 1.21.6)
+6. [OpenJDK](https://openjdk.java.net/) (OpenJDK version >= 17.0.2)
+7. [Maven](https://maven.apache.org/) (Maven version >= 3.8.4)
+8. [Docker](https://www.docker.com/) (Docker version >= 20.10.12)
+
+## Quick Start
+1. Clone the repository to local
+```
+git clone https://github.com/hdtopku/tiny-admin.git
+```
+
+
--- tiny-admin-vue/src/views/sms/hotgoods/index1.vue
+++ tiny-admin-vue/src/views/sms/hotgoods/index1.vue
@@ -1,72 +1,66 @@
-<!--<template>-->
-<!--  <div>-->
-<!--    <Search :loading="loading" :search-form="searchParams" :top="84" placeholder="搜索商品名称、备注"-->
-<!--            @open-modal="openModal" @query-list="queryList"/>-->
-<!--    <PcDataList :data-source="dataSource" :loading="loading" :pagination="pagination" class="hidden sm:block"-->
-<!--                @query-list="queryList" @change-record-status="changeRecordStatus"-->
-<!--                @delete-record-by-id="deleteRecordById"-->
-<!--                @open-modal="openModal" @open-edit-modal="openEditModal"/>-->
-<!--    <MobileDataList :data-source="dataSource" :loading="loading" :pagination="pagination" class="block sm:hidden"-->
-<!--                    @query-list="queryList" @change-record-status="changeRecordStatus"-->
-<!--                    @delete-record-by-id="deleteRecordById"-->
-<!--                    @open-modal="openModal" @open-edit-modal="openEditModal"/>-->
-<!--    <DataModal ref="modalRef" @query-list="queryList"/>-->
-<!--    <EditNewGoodsModal ref="editModalRef" @query-list="queryList"/>-->
-<!--  </div>-->
-<!--</template>-->
-<!--<script lang="ts" setup>-->
-<!--import DataModal from '@/views/sms/newgoods/DataModal.vue'-->
-<!--import {-->
-<!--  deleteSmsHotGoods,-->
-<!--  getAllHotGoodsIds,-->
-<!--  getSmsHotGoodsPage,-->
-<!--  saveHotGoods,-->
-<!--  updateHotGoods-->
-<!--} from '@/api/sms/hotGoods.ts'-->
-<!--import Search from "@/components/Search.vue";-->
-<!--import PcDataList from "@/views/sms/newgoods/PcDataList.vue"-->
-<!--import {message} from "ant-design-vue";-->
-<!--import {t} from "@/utils/i18n.ts";-->
-<!--import EditNewGoodsModal from "@/views/sms/newgoods/EditNewGoodsModal.vue";-->
-<!--import MobileDataList from "@/views/sms/newgoods/MobileDataList.vue";-->
+<template>
+  <div>
+    <Search :loading="loading" :search-form="searchParams" :top="84" placeholder="搜索商品名称、备注"
+            @open-modal="openModal" @query-list="queryList"/>
+    <PcDataList :data-source="dataSource" :loading="loading" :pagination="pagination" class="hidden sm:block"
+                @query-list="queryList" @change-record-status="changeRecordStatus"
+                @delete-record-by-id="deleteRecordById"
+                @open-modal="openModal" @open-edit-modal="openEditModal"/>
+    <MobileDataList :data-source="dataSource" :loading="loading" :pagination="pagination" class="block sm:hidden"
+                    @query-list="queryList" @change-record-status="changeRecordStatus"
+                    @delete-record-by-id="deleteRecordById"
+                    @open-modal="openModal" @open-edit-modal="openEditModal"/>
+    <DataModal ref="modalRef" @query-list="queryList"/>
+    <EditNewGoodsModal ref="editModalRef" @query-list="queryList"/>
+  </div>
+</template>
+<script lang="ts" setup>
+import DataModal from '@/views/sms/newgoods/DataModal.vue'
+import {getAllHotGoodsIds, saveHotGoods,} from '@/api/sms/hotGoods.ts'
+import Search from "@/components/Search.vue";
+import PcDataList from "@/views/sms/newgoods/PcDataList.vue"
+import {message} from "ant-design-vue";
+import {t} from "@/utils/i18n.ts";
+import EditNewGoodsModal from "@/views/sms/newgoods/EditNewGoodsModal.vue";
+import MobileDataList from "@/views/sms/newgoods/MobileDataList.vue";
 
-<!--const loading = ref(false), dataSource = ref([]), modalRef = ref(), editModalRef = ref()-->
-<!--let pagination: any = {}, searchParams: any = {keyword: '', status: true, pageNum: 1, pageSize: 10}-->
+const loading = ref(false), dataSource = ref([]), modalRef = ref(), editModalRef = ref()
+let pagination: any = {}, searchParams: any = {keyword: '', status: true, pageNum: 1, pageSize: 10}
 
-<!--const openModal = (record: any) => {-->
-<!--  modalRef.value.openModal(record, getAllHotGoodsIds, saveHotGoods)-->
-<!--}-->
-<!--const openEditModal = (record: any) => {-->
-<!--  editModalRef.value.openModal(record, updateHotGoods)-->
-<!--}-->
+const openModal = (record: any) => {
+  modalRef.value.openModal(record, getAllHotGoodsIds, saveHotGoods)
+}
+const openEditModal = (record: any) => {
+  editModalRef.value.openModal(record, updateHotGoods)
+}
 
-<!--const queryList = (params = {}) => {-->
-<!--  loading.value = true-->
-<!--  searchParams = {...searchParams, ...params}-->
-<!--  getSmsHotGoodsPage(searchParams)-->
-<!--      .then((res: any) => {-->
-<!--        dataSource.value = res.records-->
-<!--        pagination = {current: res.current, pageSize: res.size, total: res.total}-->
-<!--      })-->
-<!--      .finally(() => {-->
-<!--        loading.value = false-->
-<!--      })-->
-<!--}-->
+const queryList = (params = {}) => {
+  loading.value = true
+  searchParams = {...searchParams, ...params}
+  getSmsHotGoodsPage(searchParams)
+      .then((res: any) => {
+        dataSource.value = res.records
+        pagination = {current: res.current, pageSize: res.size, total: res.total}
+      })
+      .finally(() => {
+        loading.value = false
+      })
+}
 
-<!--const changeRecordStatus = (record: any) => {-->
-<!--  record.loading = true-->
-<!--  updateHotGoods({id: record.id, status: !record.status}).then(() => {-->
-<!--    message.success(t('操作成功'))-->
-<!--    record.status = !record.status-->
-<!--  }).finally(() => {-->
-<!--    record.loading = false-->
-<!--  })-->
-<!--}-->
+const changeRecordStatus = (record: any) => {
+  record.loading = true
+  updateHotGoods({id: record.id, status: !record.status}).then(() => {
+    message.success(t('操作成功'))
+    record.status = !record.status
+  }).finally(() => {
+    record.loading = false
+  })
+}
 
-<!--const deleteRecordById = (id: string) => {-->
-<!--  deleteSmsHotGoods(id).then(() => {-->
-<!--    message.success(t('删除成功'))-->
-<!--    queryList()-->
-<!--  })-->
-<!--}-->
-<!--</script>-->
+const deleteRecordById = (id: string) => {
+  deleteSmsHotGoods(id).then(() => {
+    message.success(t('删除成功'))
+    queryList()
+  })
+}
+</script>
